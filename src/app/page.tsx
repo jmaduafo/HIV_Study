deb5b8e5f93bac1a0a9190e109ea210db0061999
Jay Madu <jmaduafokwa@hotmail.com>
2024-10-21 14:11:36 +0100

    tweaked styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="h-full">
-       <embed src={'/assets/hiv_study.pdf'} width={"100%"} height="100%"/>
+       <embed src={'/assets/hiv_study.pdf'} className="w-full h-full" width="100%" height="100%"/>
       </main>
     </div>
   );
